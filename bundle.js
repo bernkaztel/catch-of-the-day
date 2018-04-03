30d087d84f42f9d78d90438334e646aa8e273f4b
bernkaztel <bernkaztel@hotmail.com>
2018-04-03 14:12:58 -0500

    subiendo menu mediante inventory

--- bundle.js
+++ bundle.js
@@ -21765,6 +21765,10 @@ var _Inventory = require('./Inventory');
 
 var _Inventory2 = _interopRequireDefault(_Inventory);
 
+var _sampleFishes = require('../sample-fishes');
+
+var _sampleFishes2 = _interopRequireDefault(_sampleFishes);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -21784,6 +21788,7 @@ var App = function (_React$Component) {
 
 
     _this.addFish = _this.addFish.bind(_this);
+    _this.loadSamples = _this.loadSamples.bind(_this);
     //this is the initial state
     _this.state = {
       fishes: {},
@@ -21809,6 +21814,15 @@ var App = function (_React$Component) {
       //thrid we update our state
       this.setState({ fishes: fishes });
     }
+    //to load fishes sample 
+
+  }, {
+    key: 'loadSamples',
+    value: function loadSamples() {
+      this.setState({
+        fishes: _sampleFishes2.default
+      });
+    }
   }, {
     key: 'render',
     value: function render() {
@@ -21821,7 +21835,7 @@ var App = function (_React$Component) {
           _react2.default.createElement(_Header2.default, { tagline: 'Fresh Seafood Market' })
         ),
         _react2.default.createElement(_Order2.default, null),
-        _react2.default.createElement(_Inventory2.default, { addFish: this.addFish })
+        _react2.default.createElement(_Inventory2.default, { addFish: this.addFish, loadSamples: this.loadSamples })
       );
     }
   }]);
@@ -21831,7 +21845,7 @@ var App = function (_React$Component) {
 
 exports.default = App;
 
-},{"./Header":57,"./Inventory":58,"./Order":60,"react":51}],57:[function(require,module,exports){
+},{"../sample-fishes":64,"./Header":57,"./Inventory":58,"./Order":60,"react":51}],57:[function(require,module,exports){
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
@@ -21927,7 +21941,12 @@ var Inventory = function (_React$Component) {
           null,
           'Inventory'
         ),
-        _react2.default.createElement(_AddFishForm2.default, { addFish: this.props.addFish })
+        _react2.default.createElement(_AddFishForm2.default, { addFish: this.props.addFish }),
+        _react2.default.createElement(
+          'button',
+          { onClick: this.props.loadSamples },
+          'Load Sample Fishes'
+        )
       );
     }
   }]);
@@ -22184,4 +22203,82 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 (0, _reactDom.render)(_react2.default.createElement(_App2.default, null), document.querySelector('#main'));
 
-},{"./components/App":56,"./components/NotFound":59,"./components/StorePicker":61,"react":51,"react-dom":30,"react-router":38}]},{},[63]);
+},{"./components/App":56,"./components/NotFound":59,"./components/StorePicker":61,"react":51,"react-dom":30,"react-router":38}],64:[function(require,module,exports){
+'use strict';
+
+// This is just some sample data so you don't have to think of your own!
+module.exports = {
+  fish1: {
+    name: 'Pacific Halibut',
+    image: 'http://i.istockimg.com/file_thumbview_approve/36248396/5/stock-photo-36248396-blackened-cajun-sea-bass.jpg',
+    desc: 'Everyones favorite white fish. We will cut it to the size you need and ship it.',
+    price: 1724,
+    status: 'available'
+  },
+
+  fish2: {
+    name: 'Lobster',
+    image: 'http://i.istockimg.com/file_thumbview_approve/32135274/5/stock-photo-32135274-cooked-lobster.jpg',
+    desc: 'These tender, mouth-watering beauties are a fantastic hit at any dinner party.',
+    price: 3200,
+    status: 'available'
+  },
+
+  fish3: {
+    name: 'Sea Scallops',
+    image: 'http://i.istockimg.com/file_thumbview_approve/58624176/5/stock-photo-58624176-scallops-on-black-stone-plate.jpg',
+    desc: 'Big, sweet and tender. True dry-pack scallops from the icey waters of Alaska. About 8-10 per pound',
+    price: 1684,
+    status: 'unavailable'
+  },
+
+  fish4: {
+    name: 'Mahi Mahi',
+    image: 'http://i.istockimg.com/file_thumbview_approve/12556651/5/stock-photo-12556651-mahimahi.jpg',
+    desc: 'Lean flesh with a mild, sweet flavor profile, moderately firm texture and large, moist flakes. ',
+    price: 1129,
+    status: 'available'
+  },
+
+  fish5: {
+    name: 'King Crab',
+    image: 'http://i.istockimg.com/file_thumbview_approve/18294110/5/stock-photo-18294110-king-crab-legs.jpg',
+    desc: 'Crack these open and enjoy them plain or with one of our cocktail sauces',
+    price: 4234,
+    status: 'available'
+  },
+
+  fish6: {
+    name: 'Atlantic Salmon',
+    image: 'http://i.istockimg.com/file_thumbview_approve/56241842/5/stock-photo-56241842-salmon-fish.jpg',
+    desc: 'This flaky, oily salmon is truly the king of the sea. Bake it, grill it, broil it...as good as it gets!',
+    price: 1453,
+    status: 'available'
+  },
+
+  fish7: {
+    name: 'Oysters',
+    image: 'http://i.istockimg.com/file_thumbview_approve/58626682/5/stock-photo-58626682-fresh-oysters-on-a-black-stone-plate-top-view.jpg',
+    desc: 'A soft plump oyster with a sweet salty flavor and a clean finish.',
+    price: 2543,
+    status: 'available'
+  },
+
+  fish8: {
+    name: 'Mussels',
+    image: 'http://i.istockimg.com/file_thumbview_approve/40450406/5/stock-photo-40450406-steamed-mussels.jpg',
+    desc: 'The best mussels from the Pacific Northwest with a full-flavored and complex taste.',
+    price: 425,
+    status: 'available'
+  },
+
+  fish9: {
+    name: 'Jumbo Prawns',
+    image: 'http://i.istockimg.com/file_thumbview_approve/67121439/5/stock-photo-67121439-fresh-tiger-shrimp-on-ice-on-a-black-stone-table.jpg',
+    desc: 'With 21-25 two bite prawns in each pound, these sweet morsels are perfect for shish-kabobs.',
+    price: 2250,
+    status: 'available'
+  }
+};
+
+},{}]},{},[63]);
--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes'
 
 class App extends React.Component {
   constructor(){
     //always before using this
     super();
     this.addFish = this.addFish.bind(this);
+    this.loadSamples = this.loadSamples.bind(this);
     //this is the initial state
     this.state={
       fishes: {
@@ -32,6 +34,12 @@ class App extends React.Component {
     //thrid we update our state
     this.setState({ fishes });
   }
+  //to load fishes sample 
+  loadSamples(){
+    this.setState({
+      fishes:sampleFishes
+    })
+  }
   render() {
     return (
       <div className="catch-of-the-day">
@@ -39,7 +47,7 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market" />
         </div>
         <Order />
-        <Inventory addFish={this.addFish} />
+        <Inventory addFish={this.addFish}  loadSamples={this.loadSamples}/>
       </div>
     )
   }
